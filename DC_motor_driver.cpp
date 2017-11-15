a848aaaecc68ded592c4bd219b4acfb1f5f93f4f
Harry Gee <code@pibot.org>
2017-11-15 16:21:51 +0000

    Update DC_motor_driver.cpp

--- DC_motor_driver.cpp
+++ DC_motor_driver.cpp
@@ -1,12 +1,12 @@
 #include "DC_motor_driver.h"
-const unsigned char DRV8835MotorShield::_M1DIR = 7;
-const unsigned char DRV8835MotorShield::_M2DIR = 8;
-const unsigned char DRV8835MotorShield::_M1PWM = 9;
-const unsigned char DRV8835MotorShield::_M2PWM = 10;
-boolean DRV8835MotorShield::_flipM1 = false;
-boolean DRV8835MotorShield::_flipM2 = false;
+const unsigned char DC_motor_driver::_M1DIR = 7;
+const unsigned char DC_motor_driver::_M2DIR = 8;
+const unsigned char DC_motor_driver::_M1PWM = 9;
+const unsigned char DC_motor_driver::_M2PWM = 10;
+boolean DC_motor_driver::_flipM1 = false;
+boolean DC_motor_driver::_flipM2 = false;
 
-void DRV8835MotorShield::initPinsAndMaybeTimer()
+void DC_motor_driver::initPinsAndMaybeTimer()
 {
   // Initialize the pin states used by the motor driver shield
   // digitalWrite is called before and after setting pinMode.
@@ -30,7 +30,7 @@ void DRV8835MotorShield::initPinsAndMaybeTimer()
   digitalWrite(_M2DIR, LOW);
   pinMode(_M2DIR, OUTPUT);
   digitalWrite(_M2DIR, LOW);
-#ifdef DRV8835MOTORSHIELD_USE_20KHZ_PWM
+#ifdef DC_motor_driver_USE_20KHZ_PWM
   // timer 1 configuration
   // prescaler: clockI/O / 1
   // outputs enabled
@@ -46,7 +46,7 @@ void DRV8835MotorShield::initPinsAndMaybeTimer()
 }
 
 // speed should be a number between -400 and 400
-void DRV8835MotorShield::setM1Speed(int speed)
+void DC_motor_driver::setM1Speed(int speed)
 {
   init(); // initialize if necessary
     
@@ -60,7 +60,7 @@ void DRV8835MotorShield::setM1Speed(int speed)
   if (speed > 400)  // max 
     speed = 400;
     
-#ifdef DRV8835MOTORSHIELD_USE_20KHZ_PWM
+#ifdef DC_motor_driver_USE_20KHZ_PWM
   OCR1A = speed;
 #else
   analogWrite(_M1PWM, speed * 51 / 80); // default to using analogWrite, mapping 400 to 255
@@ -73,7 +73,7 @@ void DRV8835MotorShield::setM1Speed(int speed)
 }
 
 // speed should be a number between -400 and 400
-void DRV8835MotorShield::setM2Speed(int speed)
+void DC_motor_driver::setM2Speed(int speed)
 {
   init(); // initialize if necessary
     
@@ -87,7 +87,7 @@ void DRV8835MotorShield::setM2Speed(int speed)
   if (speed > 400)  // max PWM duty cycle
     speed = 400;
     
-#ifdef DRV8835MOTORSHIELD_USE_20KHZ_PWM
+#ifdef DC_motor_driver_USE_20KHZ_PWM
   OCR1B = speed;
 #else
   analogWrite(_M2PWM, speed * 51 / 80); // default to using analogWrite, mapping 400 to 255
@@ -101,17 +101,17 @@ void DRV8835MotorShield::setM2Speed(int speed)
 
 // set speed for both motors
 // speed should be a number between -400 and 400
-void DRV8835MotorShield::setSpeeds(int m1Speed, int m2Speed){
+void DC_motor_driver::setSpeeds(int m1Speed, int m2Speed){
   setM1Speed(m1Speed);
   setM2Speed(m2Speed);
 }
 
-void DRV8835MotorShield::flipM1(boolean flip)
+void DC_motor_driver::flipM1(boolean flip)
 {
   _flipM1 = flip;
 }
 
-void DRV8835MotorShield::flipM2(boolean flip)
+void DC_motor_driver::flipM2(boolean flip)
 {
   _flipM2 = flip;
 }
